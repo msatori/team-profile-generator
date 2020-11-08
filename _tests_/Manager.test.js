d5a6578cca252f3e7bbf59eea0eb9a2387e2ff8d
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-07 21:05:07 -0600

    trial manager test passed

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -6,8 +6,12 @@ test('creates a manager object', () => {
     const manager = new Manager('Stick', 3, 'bob@bob', '55');
     
 
-    expect(manager.name).toBe('stick');
+    expect(manager.name).toBe('Stick');
     expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.officeNumber).toEqual(expect.any(String));
     expect(manager.email).toEqual(expect.any(String));
-    expect(manager.officeNumber).toEqual(expect.any(Number));
 });
+
+//create test for office umber
+//create test for role
+//create test for 
