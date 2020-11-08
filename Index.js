387257e8342659a04caac45e5a0ab8f47ce04828
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-07 22:10:47 -0600

    created intern object

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
+const Manager = require('../lib/Manager');
+const Intern = require('../lib/Intern');
+const Engineer = require('../lib/Engineer');
 const Employee = require('./lib/Employee');
 
-function newEmployee() {
-    this.employee.push(new Employee( ))
-}
